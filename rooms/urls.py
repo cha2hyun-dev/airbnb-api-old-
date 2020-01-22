4e8fe3f1b0d83dc2a536e9f394f8fb18fd9bf945
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-22 16:34:33 +0900

    #1.6 ModelViewSet

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = "rooms"
 urlpatterns = [
-    path("list/", views.ListRoomsView.as_view())
+    path("list/", views.ListRoomsView.as_view()),
+    path("<int:pk>/", views.SeeRoomView.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,18 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .serializers import RoomSerializer, BigRoomSerializer
 from .models import Room
-from .serializers import RoomSerializer
 
 
 class ListRoomsView(ListAPIView):
 
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class SeeRoomView(RetrieveAPIView):
+    queryset = Room.objects.all()
+    serializer_class = BigRoomSerializer
+
 
 # from rest_framework.decorators import api_view
 # from rest_framework.views import APIView
