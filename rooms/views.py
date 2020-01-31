b2e5086fdd15c2444032cad83b88e032a0b2e658
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-01-31 17:26:15 +0900

    #2.1 Create Room part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,33 +1,26 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from .serializers import RoomSerializer
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Room
+from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
 
-class ListRoomsView(ListAPIView):
-
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+@api_view(["GET", "POST"])
+def rooms_view(request):
+    if request.method == "GET":
+        rooms = Room.objects.all()
+        serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(serializer)
+    elif request.method == "POST":
+        serializer = WriteRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class SeeRoomView(RetrieveAPIView):
+
     queryset = Room.objects.all()
-    serializer_class = RoomSerializer
-
-
-# from rest_framework.decorators import api_view
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-
-# FUNCTION BASED VIEW
-# @api_view(["GET"])
-# def list_rooms(request):
-#     rooms = Room.objects.all()
-#     serialized_rooms = RoomSerializer(rooms, many=True)
-#     return Response(data=serialized_rooms.data)
-
-# CLASS BASED VIEW
-# class ListRoomsView(APIView):
-#     def get(self, request):
-#         rooms = Room.objects.all()
-#         serializer = RoomSerializer(rooms, many=True)
-#         return Response(serializer.data)
+    serializer_class = ReadRoomSerializer
