548c9a80c15a9891834811053d9756ad52e92754
cha2hyun <cha2hyun@cha2hyunui-MacBookPro.local>
2020-02-20 18:45:37 +0900

    #2.10 Creating Account

--- users/views.py
+++ users/views.py
@@ -5,35 +5,38 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
-from .serializers import ReadUserSerializer, WriteUserSerializer
 from .models import User
+from .serializers import UserSerializer
 
 
-class MeView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
-
-    def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+class UsersView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_200_OK)
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user).data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class MeView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response(UserSerializer(request.user).data)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FavsView(APIView):
+
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -55,3 +58,12 @@ class FavsView(APIView):
             except Room.DoesNotExist:
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
